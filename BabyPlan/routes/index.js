b8b4ee9d706ac063fbf9a6fe8c6cc6d1420d7c29
Administrator <Administrator@2013-20140406DS>
2014-05-26 22:56:19 +0800

    开关未起效

--- BabyPlan/routes/index.js
+++ BabyPlan/routes/index.js
@@ -6,3 +6,16 @@
 exports.index = function(req, res){
   res.render('index', { title: 'Express' });
 };
+
+module.exports = function(app) {
+
+	app.get('/', function(req, res) {
+		res.redirect('/index');
+	});
+	app.get('/index', function(req, res) { 
+		res.render('index', { 
+			title : 'BabyPlan',
+		});
+	});
+	
+}
