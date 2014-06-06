927bd5f91a932eea15ae91df82c17e5772b6e527
chunyu.zhao <zwl0017@163.com>
2014-06-06 05:35:37 -0400

    message

--- BabyPlan/app.js
+++ BabyPlan/app.js
@@ -12,7 +12,7 @@ var express = require('express')
 var app = express();
 
 // all environments
-app.set('port', process.env.PORT || 3500);
+app.set('port', process.env.PORT || 3501);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 app.use(express.favicon());
